0ac956d2bc298827b12258dc0e4dc2b84b8c2d4c
raalbright <ray.a.albright@gmail.com>
2018-09-24 09:22:05 -0500

    fix typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var friends = ['Ray', 'JT', 'Justin', 'Ali', 'Ron'];
 var places = ['Kitchen', 'Basement', 'Living Room', 'Dining Room', 'Den', 'Bathroom', 'Bedroom', 'Front Yard', 'Back Yard', 'Garage'];
 var weapons = ['Knife', 'Hammer', 'Gun', 'Sword', 'Weapon-5', 'Weapon-6', 'Weapon-7', 'Weapon-8', 'Weapon-9', 'Weapon-10', 'Weapon-11', 'Weapon-12', 'Weapon-13', 'Weapon-14', 'Weapon-15', 'Weapon-16', 'Weapon-17', 'Weapon-18', 'Weapon-19', 'Weapon-20'];
 
-for (var i = 0; i <= 100; i++) { 
+for (var i = 0; i < 100; i++) { 
     var h3 = document.createElement('h3');
     h3.innerText = `Accusation ${i + 1}`;
     h3.addEventListener('click', (function (n) {
